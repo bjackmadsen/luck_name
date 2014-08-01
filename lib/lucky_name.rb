4293c3236e2ff4e0e2a8d055c5ace3483512e368
Brandon Madsen <bjackmadsen@gmail.com>
2014-08-01 10:20:21 -0700

    made final changes to the programand added a README file

--- lib/lucky_name.rb
+++ lib/lucky_name.rb
@@ -1,16 +1,15 @@
 def lucky_name(name)
   name = name.upcase
   letters = name.to_s.split('')
-  letter_val = {"A" => 1, "B" => 3,  "C" => 3, "D" => 2, "E" => 1,
-    "F" => 4, "G" => 2,  "H" => 4, "I" => 1, "J" => 8,
-    "K" => 5, "L" => 1,  "M" => 3, "N" => 1, "O" => 1,
-    "P" => 3, "Q" => 10, "R" => 1, "S" => 1, "T" => 1,
-    "U" => 1, "V" => 4,  "W" => 4, "X" => 8, "Y" => 4,
-    "Z" => 10}
+  letter_val = {"A" => 2, "B" => 2,  "C" => 2, "D" => 3, "E" => 2,
+    "F" => 3, "G" => 2,  "H" => 3, "I" => 2, "J" => 3,
+    "K" => 2, "L" => 2,  "M" => 2, "N" => 2, "O" => 3,
+    "P" => 2, "Q" => 3, "R" => 2, "S" => 3, "T" => 2,
+    "U" => 3, "V" => 3,  "W" => 3, "X" => 2, "Y" => 3,
+    "Z" => 2}
   letters.map! {|x| x = letter_val[x]}
   first_half = letters.slice(0..(letters.length/2)-1)
   second_half = letters.slice(-(letters.size/2)..-1)
   first_half.inject(:+) == second_half.inject(:+)
-
 end
-puts lucky_name("bops")
+puts lucky_name("michael")
